7331d25b60c318a5de26d7554ca1dd8c953669cf
Neev1118 <76807454+Neev1118@users.noreply.github.com>
2021-05-07 14:22:04 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box {
+    constructor(x, y) {
+      var options = {
+        'density':4,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.circle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+            var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('white')
+      fill('yellow')
+      
+      circle(0, 0, this.width, this.height);
+      
+    };
+  };
+  
